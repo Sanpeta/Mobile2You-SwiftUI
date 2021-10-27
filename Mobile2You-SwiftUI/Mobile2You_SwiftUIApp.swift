f993f924f10ccb35b592063d0b83ef6d66ffa2d5
Sanpeta <sidneisonic@hotmail.com>
2021-10-27 09:02:44 -0300

    Initial Commit

---
+++ Mobile2You-SwiftUI/Mobile2You_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile2You_SwiftUIApp.swift
+//  Mobile2You-SwiftUI
+//
+//  Created by Sidnei de Souza Junior on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Mobile2You_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
